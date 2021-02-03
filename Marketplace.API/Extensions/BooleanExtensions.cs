25755473ece892f35868ec498a16e25b4df2e930
Houston Nguyen <16967432+HoustonNguyen@users.noreply.github.com>
2021-02-03 03:56:02 -0500

    Create BooleanExtensions.cs
    Mark asked me about String extensions so I included this really simple one. The need came, so I might as well.
    
    Just converts a bool to Yes or No to make it more user friendly.

---
+++ Marketplace.API/Extensions/BooleanExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.API.Extensions
+{
+    public static class BooleanExtensions
+    {
+        public static string ToYesOrNo(this bool value)
+        {
+            return value ? "Yes" : "No";
+        }
+    }
+}
