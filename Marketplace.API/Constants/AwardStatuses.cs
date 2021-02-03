18fa70c7ee8aae019dcfe161b009f312af86041d
Houston Nguyen <16967432+HoustonNguyen@users.noreply.github.com>
2021-02-03 11:24:08 -0500

    Create AwardStatuses.cs
    Added a constants file

---
+++ Marketplace.API/Constants/AwardStatuses.cs
@@ -0,0 +1,8 @@
+﻿namespace Marketplace.API.Constants
+{
+    public static class AwardStatuses
+    {
+        public static readonly string NOMINATED = "Nominated";
+        public static readonly string WON = "Won";
+    }
+}
